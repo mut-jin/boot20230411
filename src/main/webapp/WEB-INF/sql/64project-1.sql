da39dbaca0fb2203123dd9ed1c7fec81eee9f77a
mut-jin <dkssud2422@naver.com>
2023-05-15 16:36:34 +0900

    20230515

--- src/main/webapp/WEB-INF/sql/64project-1.sql
+++ src/main/webapp/WEB-INF/sql/64project-1.sql
@@ -1,2 +1,42 @@
 USE Board;
 SELECT * FROM Board;
+SELECT * FROM Member;
+
+UPDATE Board
+SET writer = '1234';
+
+ALTER TABLE Board
+ADD FOREIGN KEY (writer) REFERENCES Member(id);
+
+
+SELECT * FROM Member;
+
+-- 권한 테이블
+CREATE TABLE MemberAuthority (
+    memberId VARCHAR(20) NOT NULL,
+	authority VARCHAR(30) NOT NULL,
+    FOREIGN KEY (memberId) REFERENCES Member(id),
+    PRIMARY KEY (memberId, authority)
+);
+
+SELECT * FROM MemberAuthority;
+
+INSERT INTO MemberAuthority
+VALUES ('admin0', 'admin');
+
+DESC Member;
+
+SELECT *
+FROM Member m LEFT JOIN MemberAuthority ma ON m.id = ma.memberId
+WHERE m.id = 'admin0';
+
+
+
+
+
+
+
+
+
+
+
